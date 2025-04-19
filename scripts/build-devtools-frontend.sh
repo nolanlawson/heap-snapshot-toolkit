377c58ed9cc126f421881cf7e6c67f61cf5c7f4c
Nolan Lawson <nolan@nolanlawson.com>
2025-04-19 11:31:28 -0700

    chore: rename (#2)

--- scripts/build-devtools-frontend.sh
+++ scripts/build-devtools-frontend.sh
@@ -5,11 +5,11 @@ set -x
 
 # build devtools-frontend with rollup
 cd ../devtools-frontend
-node --max-old-space-size=4096 ../heap-snapshot-parser/node_modules/rollup/dist/bin/rollup \
-    --config ../heap-snapshot-parser/scripts/devtools.rollup.config.js
+node --max-old-space-size=4096 ../heap-snapshot-toolkit/node_modules/rollup/dist/bin/rollup \
+    --config ../heap-snapshot-toolkit/scripts/devtools.rollup.config.js
 
 # remove unused js/tsbuildinfo files
-cd ../heap-snapshot-parser
+cd ../heap-snapshot-toolkit
 rm -fr thirdparty/devtools-frontend/{front_end,inspector_overlay,test,tsconfig.tsbuildinfo}
 
 # remove unused .d.ts files since devtools-frontend has a lot
